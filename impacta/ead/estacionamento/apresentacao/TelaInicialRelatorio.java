dbfb1a8f86774d1a8257b87604748b61f5455e3a
Guilherme Barbosa <gbb090977@gmail.com>
2024-07-09 22:43:13 -0300

    Relatório de faturamento: Apresentação

--- impacta/ead/estacionamento/apresentacao/TelaInicialRelatorio.java
+++ impacta/ead/estacionamento/apresentacao/TelaInicialRelatorio.java
@@ -1,7 +1,77 @@
 package impacta.ead.estacionamento.apresentacao;
 
-import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
+import java.util.List;
 
-public class TelaInicialRelatorio extends JFrame {
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import impacta.ead.estacionamento.controle.EstacionamentoController;
+import impacta.ead.estacionamento.negocio.Movimentacao;
+
+public class TelaInicialRelatorio extends JFrame implements ActionListener{
+	
+	private JComboBox cboAno;
+	private JComboBox cboMes;
+	
+	public TelaInicialRelatorio() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(new Dimension(600,140));
+		setResizable(false);
+		setTitle("Filtro do relatório");
+		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 15, 40));
+		
+		JLabel lblAno = new JLabel("Ano:");
+		lblAno.setFont(new Font("Tahoma", Font.BOLD, 14));
+		getContentPane().add(lblAno);
+		
+		cboAno = new JComboBox();
+		cboAno.setModel(new DefaultComboBoxModel(new String[] {"2027", "2026", "2025", "2024", "2023", "2022", "2021", "2020"}));
+		cboAno.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		getContentPane().add(cboAno);
+		
+		JLabel lblMes = new JLabel("Mês:");
+		lblMes.setFont(new Font("Tahoma", Font.BOLD, 14));
+		getContentPane().add(lblMes);
+		
+		cboMes = new JComboBox();
+		cboMes.setModel(new DefaultComboBoxModel(new String[] {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"}));
+		cboMes.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		getContentPane().add(cboMes);
+		
+		JButton btnGerar = new JButton("Gerar");
+		btnGerar.addActionListener(this);
+		btnGerar.setFont(new Font("Tahoma", Font.BOLD, 14));
+		getContentPane().add(btnGerar);
+		
+		setLocationRelativeTo(null);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evento) {
+		//recupera do combo o ano e mes escolhido
+		int ano = (Integer) cboAno.getSelectedItem();
+		int mes = (Integer) cboMes.getSelectedIndex()+1;
+		
+		//buscar as movimentações do ano e mês informados
+		EstacionamentoController controle = new EstacionamentoController();
+		LocalDateTime data = LocalDateTime.of(1, mes, 1,0,0);
+		List<Movimentacao>movimentacoes = controle.emitirRelatorio(data);
+		
+		//Exibe a tela de conteudo e faturamento
+		TelaResultadoRelatorio relatorio = new TelaResultadoRelatorio(this,movimentacoes);
+		
+		relatorio.setVisible(true);
+		dispose();
+		
+	}
 
 }
--- impacta/ead/estacionamento/apresentacao/TelaResultadoRelatorio.java
+++ impacta/ead/estacionamento/apresentacao/TelaResultadoRelatorio.java
@@ -1,5 +1,71 @@
 package impacta.ead.estacionamento.apresentacao;
 
-public class TelaResultadoRelatorio {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import impacta.ead.estacionamento.negocio.Movimentacao;
+
+public class TelaResultadoRelatorio extends JFrame implements ActionListener{
+	private JTable tblFaturamento;
+	private JFrame parent;
+
+	public TelaResultadoRelatorio(TelaInicialRelatorio telaInicialRelatorio, List<Movimentacao> movimentacoes) {
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		setSize(new Dimension(600,300));
+		setResizable(false);
+		setTitle("Relatorio de Faturamento");
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.SOUTH);
+		
+		JButton btnOk = new JButton("Ok");
+		panel.add(btnOk);
+		
+		JPanel panel_1 = new JPanel();
+		getContentPane().add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		panel_1.add(panel_2, BorderLayout.SOUTH);
+		
+		JLabel lblTextoFaturamento = new JLabel("[Faturamento]");
+		lblTextoFaturamento.setFont(new Font("Tahoma", Font.BOLD, 14));
+		panel_2.add(lblTextoFaturamento);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel_1.add(scrollPane, BorderLayout.CENTER);
+		
+		tblFaturamento = new JTable();
+		tblFaturamento.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+					"Placa","Entrada","Sa\u00EDda","Valor"   
+			}
+		));
+		scrollPane.setViewportView(tblFaturamento);
+		// TODO Auto-generated constructor stub
+		
+		setLocationRelativeTo(null);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evento) {
+		parent.setVisible(true);
+		dispose();
+		
+	}
 
 }
